1063abb29628f8dd306efbabae926782f75125a2
cadkinscc <54828134+cadkinscc@users.noreply.github.com>
2020-02-28 19:53:38 -0700

    Update secure-ssh.sh

--- linux/centos7/secure-ssh.sh
+++ linux/centos7/secure-ssh.sh
@@ -1,1 +1,8 @@
-echo "All YOUR CODE GOES HERE"
+
+useradd -m -d /home/sshuser -s /bin/bash sshuser
+mkdir /home/sshuser/.ssh
+cp /home/chirs/.ssh/id_rsa.pub /home/sshuser/.ssh/authorize$
+chmod 700  /home/sshuser/.ssh
+chmod 600 /home/sshuser/.ssh/authorized_keys
+chown -R sshuser:sshuser /home/sshuser./ssh
+
